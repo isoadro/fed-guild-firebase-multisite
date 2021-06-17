7965158c6b4bded9bbe0bb0ca8d8e641991bc25f
isoadro <75279359+isoadro@users.noreply.github.com>
2021-06-17 14:59:16 +1000

    beans

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to my landing page</p>
+        <p>Welcome to my landing page 2</p>
       </header>
     </div>
   )
